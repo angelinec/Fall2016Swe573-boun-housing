6d761697404901f7a4131cb74c62dc12310164a6
SALIHDEMIR\Salih Demir <salihdemir.58@gmail.com>
2017-10-01 20:15:55 +0300

    no message

--- docs/main.js
+++ docs/main.js
@@ -5,8 +5,8 @@ var width = movementStrength / $(window).width();
 $("#top-image").mousemove(function(e){
           var pageX = e.pageX - ($(window).width() / 2);
           var pageY = e.pageY - ($(window).height() / 2);
-          var newvalueX = width * pageX * -1 - 25;
-          var newvalueY = height * pageY * -1 - 50;
+          var newvalueX = width * pageX * -1 - 100;
+          var newvalueY = height * pageY * -1 - 100;
           $('#top-image').css("background-position", newvalueX+"px     "+newvalueY+"px");
 });
 });
